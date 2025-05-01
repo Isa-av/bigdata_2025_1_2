9dcbc3544261faad318cbb1890c2353556477b5c
Isabela Arango <isabela.arango@est.iudigital.edu.co>
2025-05-01 00:46:06 +0000

    Actualizacion de archivo yml y creacion de la estructura del proyecto

---
+++ main.py
@@ -0,0 +1,1 @@
+from dataweb import DataWeb
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="edu_bigdata",
+    author="Isabela Arango Verona",
+    author_email="isabela.arango@est.iudigital.edu.co",
+    description="",
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "request",
+        "BeautifulSoup4"
+    ]
+)
